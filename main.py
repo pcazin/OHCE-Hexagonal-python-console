09c2c5ccb48ea7d8d995c55fda88553a9c8c80a7
pcazin <pablocazin.dev@gmail.com>
2024-03-19 14:32:11 +0100

    finished

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from app_class.MirrorApp import MirrorApp
 
 def main():
     translator = Translator('languages.json')
-    lang = input("Choose a language (English/French): ").lower()
+    lang = ""
+
+    while lang not in list(translator.languages.keys()):
+        primary_keys = [key for key in translator.languages.keys()]
+        formatted_keys = "/".join(primary_keys)  
+        lang = input(f"Choose a language ({formatted_keys}) ")
+
     app = MirrorApp(translator)
     app.run(lang)
 
